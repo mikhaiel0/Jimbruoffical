2a2a91a488907c83511ca79da9277d33f67bb5bf
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-11-02 20:42:18 +0530

    `Jimbrootan`

--- helpers.js
+++ helpers.js
@@ -1,29 +1,17 @@
 /*
-# Copyright (C) 2020 MuhammedKpln.
-#
-# WhatsAsena is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# WhatsAsena is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
+Copyright (C) 2021 Mikhaiel.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "🩸 *Jimbrootan*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🩸 *Jimbrootan*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "🩸 *Jimbrootan*:  ```" + msg + "```"
 }
 
 
