df40c519c390696d33179123988fd67c8acae630
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-11-02 20:36:21 +0530

    Update and rename pinky.js to jimbroottan.js

---
+++ jimbroottan.js
@@ -0,0 +1,19 @@
+/* 
+Special thanks to afnanplk
+*/
+
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// Jimbrootan Special Functions
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+ MKLO: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Mikhaiel is my creator' : process.env.DEPLOYER,
+ PHONE: process.env.NUMBER === undefined ? '+919544846609' : process.env.NUMBER,    
+
+};
