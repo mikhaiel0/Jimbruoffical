e5261d62357344369bc264f178ea9ebf7d36aecb
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-11-05 17:16:14 +0530

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,33 @@
+/* 
+Copyright (C) 2021 Mikhaiel.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License
+*/
+
+const Asena = require('../events');
+const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const Config = require('../config')
+const axios = require('axios')
+const request = require('request');
+const os = require('os');
+const Language = require('../language');
+const MENU_LIST = "Menu list"
+var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
+var ggg = Buffer.from(clh.cd, 'base64')
+var ddd = ggg.toString('utf-8')
+
+Asena.addCommand({pattern: 'helper ?(.*)', fromMe: true, desc:MENU_LIST}, (async (message, match) => {
+// send a list message!
+    const buttons = [
+  {buttonId: 'id1', buttonText: {displayText: '.list'}, type: 1},
+  {buttonId: 'id2', buttonText: {displayText: '.menu'}, type: 1}
+]
+const buttonMessage = {
+    contentText: "Jimbrootan offical",
+    footerText: 'Mikhaiel',
+    buttons: buttons,
+    headerType: 1
+}
+await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage)
+    }));
