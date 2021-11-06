27a12516cf91ecac585ef0de514a05dee494d44a
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-11-06 09:54:43 +0530

    Update listmessage.js

--- plugins/listmessage.js
+++ plugins/listmessage.js
@@ -16,17 +16,17 @@ var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
 
 
-Jimbru.addCommand({pattern: 'main', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+Jimbru.addCommand({pattern: 'Download', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 // send a list message!
     const rows = [
-        {title: 'Download Menu', description: ".song", rowId:"rowid1"},
+        {title: 'Download Menu', description: "╭────────────●●►\n│💰 *DOWNLOAD COMMANDS*\n│      ──────── \n │► .video [ Yt Link] \n │► .mp4    [Name] \n │► .audio  [yt link ] \n │► .download  [status download] \n │► .song  [ song name ] \n │► .dcsong [ song name ] \n │► .img [ name ] \n │► .fb  link ] \n │► .tiktok [ link ] \n │► .vinsta  [ Insta Video ] \n │► .pinsta [ Insta Post ] \n │► .twt [twitter video Link] \n │► .mediafire \n ╰────────────●●►", rowId:"rowid1"},
                  ]
        
-       const sections = [{title: "DOWNLOAD MENU", rows: rows}]
+       const sections = [{title: "𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐑 𝐌𝐄𝐍𝐔", rows: rows}]
        
        const button = {
-        buttonText: 'CLICK',
-        description: "JIMBROOTTAN",
+        buttonText: '𝐂𝐋𝐈𝐂𝐊',
+        description: "𝑱𝑰𝑴𝑩𝑹𝑶𝑶𝑻𝑨𝑵",
         sections: sections,
         listType: 1
        }
