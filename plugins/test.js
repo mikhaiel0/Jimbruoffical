bb8617c4f202f3610ee8a59d89bc428f63c384c0
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-11-07 10:29:59 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -19,7 +19,7 @@ var ddd = ggg.toString('utf-8')
 let wk = Config.WORKTYPE == 'public' ? false : true
 
 
-Asena.addCommand({pattern: 'helper ?(.*)', fromMe:wk , desc:MENU_LIST}, (async (message, match) => {
+Asena.addCommand({pattern: 'main ?(.*)', fromMe:wk , desc:MENU_LIST}, (async (message, match) => {
 // send a list message!
     const buttons = [
   {buttonId: 'id1', buttonText: {displayText: '.list'}, type: 1},
@@ -33,3 +33,21 @@ const buttonMessage = {
 }
 await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage)
     }));
+
+Asena.addCommand({pattern: "downloader ?(.*)",fromMe: wk,desc: Lang.WELCOME_DESC,onlyGroup: true,}, async (message, match) => {
+    let hg = await getMessage(message.jid, "downloader");
+    if (hg === false && match === "")
+      return await message.sendMessage(
+        genButtons(["test", "downlaod"], Lang.ON_OFF.format('downloader'), "Choose"),
+        {},
+        MessageType.buttonsMessage
+      );
+    }
+    if (match === "") return await message.sendMessage(Lang.NEED_WELCOME_TEXT);
+    else if (match == "test" || match == "download") {
+      await enableGreetings(message.jid, "downloder", match);
+      return await message.sendMessage(
+        Lang.MENU_ENABLED.format(match == "test" ? Lang.ENABLE_MENU : Lang.DISABLE_MENU)
+      );
+  }
+);
