6f0d24d7d29730b67a5597b69a30bf9aea87dbdc
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-11-08 10:47:21 +0530

    `🦠`

--- plugins/tpack.js
+++ plugins/tpack.js
@@ -2,7 +2,7 @@
 mikhaiel
 */
 
-const XTroid = require('../events');
+const Jimbroottan = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
@@ -10,12 +10,12 @@ const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('ttp');
-const Desk = "Temp logo pack \n\n╭┅🌸 *.tmug*\n╭┅🌸 *.tlove*\n╭┅🌸 *.tnaru*\n╭┅🌸 *.tgrass*\n╭┅🌸 *.tsky*\n╭┅🌸 *.tcof*\n╭┅🌸 *.tneon*\n╭┅🌸 *.tpubg*\n╭┅🌸 *.tbit*\n```.tbit text1,text2```\n╭┅🌸 *.tbt*\n```.tbt text1,text2```\n"
+const Desk = "Temp logo pack \n\n╭┅🦠 *.tmug*\n╭┅🦠 *.tlove*\n╭┅🦠 *.tnaru*\n╭┅🦠 *.tgrass*\n╭┅🦠 *.tsky*\n╭┅🦠 *.tcof*\n╭┅🦠 *.tneon*\n╭┅🦠 *.tpubg*\n╭┅🦠 *.tbit*\n```.tbit text1,text2```\n╭┅🦠 *.tbt*\n```.tbt text1,text2```\n"
 
 if (Config.WORKTYPE == 'private') {
 
 
-    XTroid.addCMD({ pattern: 'tpack ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tpack ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
         await message.sendMessage(Desk);
         
     }));
@@ -24,27 +24,27 @@ if (Config.WORKTYPE == 'private') {
 
 
 
-    XTroid.addCMD({ pattern: 'tmug ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tmug ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/candlemug/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
 
     }));
 
-    XTroid.addCMD({ pattern: 'tlove ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tlove ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/lovemsg/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
 
     }));
 
-    XTroid.addCMD({ pattern: 'tnaru ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tnaru ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
@@ -54,58 +54,58 @@ if (Config.WORKTYPE == 'private') {
 
     }));
 
-    XTroid.addCMD({ pattern: 'tgrass ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tgrass ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/paperonglass/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
 
     }));
 
-    XTroid.addCMD({ pattern: 'tsky ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tsky ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/shadowtext/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
 
     }));
 
-    XTroid.addCMD({ pattern: 'tcof ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tcof ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/coffeecup/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
 
     }));
 
-    XTroid.addCMD({ pattern: 'tneon ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tneon ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/glowingneon/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
 
     }));
 
-    XTroid.addCMD({ pattern: 'tpubg ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tpubg ?(.*)', fromMe: true,dontAddCMDList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/pubgmlogo/?text1=${encodeURIComponent(match[1])}&text2=X-Troid`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
 
     }));
 
 
-    XTroid.addCMD({pattern: 'tbit ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
+    Jimbroottan.addCommand({pattern: 'tbit ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
       
@@ -118,11 +118,11 @@ if (Config.WORKTYPE == 'private') {
     
         var webimage = await axios.get(`https://videfikri.com/api/textmaker/8bit/?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
     
-        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.JINN})
     
         }));
 
-        XTroid.addCMD({pattern: 'tbt ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
+        Jimbroottan.addCommand({pattern: 'tbt ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
 
             if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
           
@@ -135,7 +135,7 @@ if (Config.WORKTYPE == 'private') {
         
             var webimage = await axios.get(`https://videfikri.com/api/textmaker/bf4/?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
         
-            await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+            await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.JINN})
         
             }));
 
@@ -148,93 +148,93 @@ if (Config.WORKTYPE == 'private') {
 if (Config.WORKTYPE == 'public') {
 
 
-    XTroid.addCMD({ pattern: 'tpack ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tpack ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
         await message.sendMessage(Desk);
         
     }));
 
-    XTroid.addCMD({ pattern: 'tmug ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tmug ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
     
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/candlemug/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
     
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
     
     }));
     
-    XTroid.addCMD({ pattern: 'tlove ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tlove ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
     
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
     
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/lovemsg/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
     
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
     
     }));
     
-    XTroid.addCMD({ pattern: 'tnaru ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tnaru ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
     
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
     
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/narutobanner/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
     
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
     
     }));
     
-    XTroid.addCMD({ pattern: 'tgrass ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tgrass ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
     
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
     
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/paperonglass/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
     
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
     
     }));
     
-    XTroid.addCMD({ pattern: 'tsky ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tsky ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
     
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
     
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/shadowtext/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
     
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
     
     }));
     
-    XTroid.addCMD({ pattern: 'tcof ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tcof ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
     
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
     
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/coffeecup/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
     
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
     
     }));
     
-    XTroid.addCMD({ pattern: 'tneon ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tneon ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
     
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
     
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/glowingneon/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
     
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
     
     }));
     
-    XTroid.addCMD({ pattern: 'tneon ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
+    Jimbroottan.addCommand({ pattern: 'tneon ?(.*)', fromMe: false,dontAddCMDList: true }, (async (message, match) => {
     
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
     
         var lasiyasimg = await axios.get(`https://videfikri.com/api/textmaker/pubgmlogo/?text1=${encodeURIComponent(match[1])}&text2=X-Troid`, { responseType: 'arraybuffer' })
     
-        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(lasiyasimg.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.JINN})
     
     }));
     
     
-    XTroid.addCMD({pattern: 'tbit ?(.*)', fromMe: false, dontAddCMDList: true}, (async (message, match) => {
+    Jimbroottan.addCommand({pattern: 'tbit ?(.*)', fromMe: false, dontAddCMDList: true}, (async (message, match) => {
     
         if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
       
@@ -247,11 +247,11 @@ if (Config.WORKTYPE == 'public') {
     
         var webimage = await axios.get(`https://videfikri.com/api/textmaker/8bit/?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
     
-        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.JINN})
     
         }));
     
-        XTroid.addCMD({pattern: 'tbt ?(.*)', fromMe: false, dontAddCMDList: true}, (async (message, match) => {
+        Jimbroottan.addCommand({pattern: 'tbt ?(.*)', fromMe: false, dontAddCMDList: true}, (async (message, match) => {
     
             if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
           
@@ -264,7 +264,7 @@ if (Config.WORKTYPE == 'public') {
         
             var webimage = await axios.get(`https://videfikri.com/api/textmaker/bf4/?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
         
-            await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+            await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.JINN})
         
             }));
         }    
