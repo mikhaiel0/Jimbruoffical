b285d115312ea90a1799c950403d9bfa6fea66db
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-11-11 19:13:28 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -324,9 +324,9 @@ ${chalk.blue.italic('🧞 Connecting to WhatsApp...')}`);
         }
 // ════════════════════ NO ONLINE◽◽◽◽◽ 
    const type = Object.keys(msg.message)[0]
-      const selectedButton = (type == 'buttonsResponseMessage') ? msg.message.buttonsResponseMessage.selectedButtonId : ''
+      const selectedButton = (type == 'buttonsResponseMessage') ? msg.message.buttonsResponseMessage.selectedButtonId : '.menu'
       
-    if (selectedButton = (type == 'buttonsResponseMessage') ? mek.message.buttonsResponseMessage.selectedButtonId : '.help') {
+    if (selectedButton = (type == 'buttonsResponseMessage') ? mek.message.buttonsResponseMessage.selectedButtonId : '.list') {
       
       await conn.sendMessage(msg.key.remoteJid,'button 🌚', MessageType.text);
      
