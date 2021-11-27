75f7e6f55f895d31e3fad64137bca9dc7177663b
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-11-27 10:48:01 +0530

    Update jimbroottan.js

--- jimbroottan.js
+++ jimbroottan.js
@@ -16,4 +16,7 @@ module.exports = {
  OA_NAME: process.env.DEPLOYER === undefined ? 'Mikhaiel' : process.env.DEPLOYER,
  PHONE: process.env.NUMBER === undefined ? '+919544846609' : process.env.NUMBER,    
  IG_USER: process.env.IG_USER === undefined ? '@the_real_mikhaiel' : process.env.IG_USER,
+ BTN1: process.env.BTN1 === undefined ? 'Button one' : process.env.BTN1,
+ BTN2: process.env.BTN2 === undefined ? 'Button two' : process.env.BTN2,
+
 };
