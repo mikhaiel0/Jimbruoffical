9f66e8c4517f76d25a44a2c9068acc195ad424ad
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-11-27 11:20:08 +0530

    `ενημέρωση κουμπιού`

--- bot.js
+++ bot.js
@@ -11,7 +11,6 @@ const path = require("path");
 const events = require("./events");
 const chalk = require('chalk');
 const config = require('./config');
-const CRelpy = require('../jimbroottan');
 const simpleGit = require('simple-git');
 const {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
 const {Message, StringSession, Image, Video} = require('./jimbru/');
@@ -332,12 +331,12 @@ const _0x2ba249=_0x3085;function _0x315e(){const _0x1721af=['1480084snwaPr','63Q
 switch(Button) {
    case '#list':
 
- await conn.sendMessage(msg.key.remoteJid, CReply.listbtn, MessageType.text);
+ await conn.sendMessage(msg.key.remoteJid, config.listbtn, MessageType.text);
 
 return;
       case '#menu':
 
- await conn.sendMessage(msg.key.remoteJid, CReply.menubtn, MessageType.text);
+ await conn.sendMessage(msg.key.remoteJid, config.menubtn, MessageType.text);
 
 return;
       case 'menu':
